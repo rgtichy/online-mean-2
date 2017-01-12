a41907845079de0a9aa9f6690306173f2867bc1d
globedasher <globedasher@localhost.localdomain>
2017-01-12 15:02:10 -0800

    Richard's additions

--- 01-03-angular-items/server.js
+++ 01-03-angular-items/server.js
@@ -2,5 +2,6 @@ var express = require('express');
 var path = require('path');
 var app = express();
 app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 
 app.listen(8000);
