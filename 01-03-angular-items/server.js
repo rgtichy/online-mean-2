b3039d07f75d987f1187db638ae92616a5769955
meadch <meadch@gmail.com>
2017-01-12 16:04:04 -0800

    begins delete functionality

--- 01-03-angular-items/server.js
+++ 01-03-angular-items/server.js
@@ -2,6 +2,6 @@ var express = require('express');
 var path = require('path');
 var app = express();
 app.use(express.static(path.join(__dirname, 'client')));
-app.use(express.static(path.join(__dirname, 'bower_components')));
+app.use('/js', express.static(path.join(__dirname, 'bower_components')));
 
 app.listen(8000);
